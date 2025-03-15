a5d453507effceaef9a12c037a0e93c984ce3a51
Zo-Tsilavina <hei.tsilavina@gmail.com>
2025-03-15 14:29:25 +0300

    feat: add splash screen for MusicMate

--- android/app/src/main/java/com/examen/MainActivity.kt
+++ android/app/src/main/java/com/examen/MainActivity.kt
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
+import java.io.File
+import android.os.Bundle
 
 class MainActivity : ReactActivity() {
 
